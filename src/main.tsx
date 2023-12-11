ea0b69a1fc971bb139e11d13ec2a4be07b4376c3
Allan de Melo Heil <allanmheil@hotmail.com>
2023-12-11 16:47:35 -0300

    Reorganizando componentes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './App';
-import { CustomThemeProvider } from '@/contexts/ThemeProvider';
+import { CustomThemeProvider } from '@/contexts/CustomThemeProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
