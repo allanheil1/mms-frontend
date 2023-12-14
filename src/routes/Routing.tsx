3801f2bac749dcb973c7bb6dc0bce3ed32d90e7f
Allan de Melo Heil <allanmheil@hotmail.com>
2023-12-14 14:45:45 -0300

    MainDrawer exemplo e ajuste de padding

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Register from '@/pages/Auth/Register';
 import Login from '@/pages/Auth/Login';
 import Home from '@/pages/Home/Home';
+import MainDrawer from '@/components/MainDrawer/MainDrawer';
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<MainDrawer> <Home /> </MainDrawer>} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
